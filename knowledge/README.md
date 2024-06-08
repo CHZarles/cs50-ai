996117d11a6bbd381f2aaab2ed9b14fb50972650
Your Name <you@example.com>
2024-06-08 18:04:49 +0800

    finish knight.md

--- knowledge/README.md
+++ knowledge/README.md
@@ -4,7 +4,10 @@
 
 # knights
 - lecture
- - https://cs50.harvard.edu/ai/2024/notes/1/
+    - https://cs50.harvard.edu/ai/2024/notes/1/
 - project url
- - https://cs50.harvard.edu/ai/2024/projects/1/knights/
+     - https://cs50.harvard.edu/ai/2024/projects/1/knights/
+- mark
+    这个project主要是训练用 Proposition 表达 knowledge base 的能力
+
 
--- knowledge/knights/puzzle.py
+++ knowledge/knights/puzzle.py
@@ -11,31 +11,61 @@ CKnave = Symbol("C is a Knave")
 
 # Puzzle 0
 # A says "I am both a knight and a knave."
+Asentence0 = And(AKnight, AKnave)
 knowledge0 = And(
     # TODO
+    Implication(AKnight , Asentence0),
+    # Implication(AKnave, Not(Asentence)), # how about remove this 
+    Biconditional(AKnight , Not(AKnave)) 
 )
 
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
+Asentence1 = And(AKnave , BKnave)
 knowledge1 = And(
     # TODO
+    Biconditional(AKnight , Not(AKnave)) ,
+    Biconditional(BKnight , Not(BKnave)) ,
+    Biconditional(AKnight , Asentence1),
 )
 
 # Puzzle 2
 # A says "We are the same kind."
 # B says "We are of different kinds."
+Asentence2 = Or(And(AKnave , BKnave) , And(AKnight ,BKnight))
+Bsentence2 = Or(And(AKnight , BKnave) , And(AKnave ,BKnight))
 knowledge2 = And(
     # TODO
+    Biconditional(AKnight , Not(AKnave)) ,
+    Biconditional(BKnight , Not(BKnave)) ,
+    Biconditional(AKnight , Asentence2),
+    Biconditional(BKnight , Bsentence2),
 )
 
 # Puzzle 3
-# A says either "I am a knight." or "I am a knave.", but you don't know which.
-# B says "A said 'I am a knave'."
+# A says either "I am a knight." or "I am a knave.", but you don't know which. 
+# B says "A said 'I am a knave'." 
 # B says "C is a knave."
 # C says "A is a knight."
-knowledge3 = And(
+
+knowledge3 = And( # dont't know
+    Biconditional(BKnight , Not(BKnave)) ,
+    Biconditional(CKnight , Not(CKnave)) ,
+    Biconditional(AKnight , Not(AKnave)) ,
+
     # TODO
+    Or(
+    And(AKnight, # A said I am a knight
+    Biconditional(BKnight , AKnave),
+    Biconditional(BKnight , CKnave),
+    Biconditional(CKnight , AKnight)) ,
+    And(AKnave, 
+    Biconditional(AKnave, Not(AKnave)),
+    Biconditional(BKnight , AKnave),
+    Biconditional(BKnight , CKnave),
+    Biconditional(CKnight , AKnight)) 
+    )
 )
 
 
@@ -45,7 +75,7 @@ def main():
         ("Puzzle 0", knowledge0),
         ("Puzzle 1", knowledge1),
         ("Puzzle 2", knowledge2),
-        ("Puzzle 3", knowledge3)
+        ("Puzzle 3", knowledge3),
     ]
     for puzzle, knowledge in puzzles:
         print(puzzle)
